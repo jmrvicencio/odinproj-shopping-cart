1f7ed221ac4f8d1e768538699cd6bcd2d71dfe1a
jmrvicencio <35318735+jmrvicencio@users.noreply.github.com>
2025-07-07 15:47:04 +0800

    💄 Added placeholder during loading games from api

--- src/components/Store.tsx
+++ src/components/Store.tsx
@@ -1,5 +1,6 @@
 import { data, Outlet, useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
+import { LoaderCircle, Circle } from "lucide-react";
 import GameItem from "./GameItem";
 
 const Store = () => {
@@ -17,10 +18,24 @@ const Store = () => {
 
   return (
     <>
-      <div className="sm: grid grow-1 grid-cols-[repeat(auto-fit,_minmax(250px,1fr))] justify-items-stretch gap-4 p-12">
-        {games?.data.map((game: { name: string; background_image: string; genres: Array<{ name: string }>; id: string }) => (
-          <GameItem key={game.id} title={game.name} image={game.background_image} genre={game.genres[0]?.name} />
-        ))}
+      <div className="grid grow-1 grid-cols-[repeat(auto-fit,_minmax(250px,1fr))] justify-items-stretch gap-4 p-12">
+        {isLoading ? (
+          <>
+            {Array.from({ length: 20 }, (_, i) => (
+              <div className="flex animate-pulse flex-col gap-2 pb-4">
+                <div className="aspect-16/9 w-full rounded-2xl bg-slate-600" />
+                <div className="h-4 w-3/4 rounded-full bg-slate-600" />
+                <div className="h-3 w-1/3 rounded-full bg-slate-600" />
+              </div>
+            ))}
+          </>
+        ) : (
+          <>
+            {games?.data.map((game: { name: string; background_image: string; genres: Array<{ name: string }>; id: string }) => (
+              <GameItem key={game.id} title={game.name} image={game.background_image} genre={game.genres[0]?.name} />
+            ))}
+          </>
+        )}
       </div>
     </>
   );
